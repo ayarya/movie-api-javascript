c96fe96111c6d3f4d4a60993dc8661e01936bb98
Ayush Arya <arya.ayush409@gmail.com>
2020-05-16 02:55:17 -0400

    Single Movie data fetched

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '374635d9',
+			i: 'tt0848228'
+		}
+	});
+	console.log('sdds', response.data);
+};
+
+fetchData();
